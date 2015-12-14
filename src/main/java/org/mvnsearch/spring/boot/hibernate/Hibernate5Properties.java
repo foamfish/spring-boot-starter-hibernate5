ce7604769169821d28b5fa9b2861b05650374550
linux_china <linux_china@hotmail.com>
2015-12-14 12:13:58 +0800

    hibernate properties

---
+++ src/main/java/org/mvnsearch/spring/boot/hibernate/Hibernate5Properties.java
@@ -0,0 +1,26 @@
+package org.mvnsearch.spring.boot.hibernate;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * hibernate properties
+ *
+ * @author linux_china
+ */
+@ConfigurationProperties(
+        prefix = "spring.hibernate5"
+)
+public class Hibernate5Properties {
+    /**
+     * hibernate config
+     */
+    private String config = "/hibernate.cfg.xml";
+
+    public String getConfig() {
+        return config;
+    }
+
+    public void setConfig(String config) {
+        this.config = config;
+    }
+}
