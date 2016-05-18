5783afc7fc1b9ec0c21ac30a76db7486b998d33b
linux_china <linux_china@hotmail.com>
2016-05-18 10:41:44 +0800

    hibernate test application

---
+++ src/test/java/org/mvnsearch/spring/boot/hibernate/demo/HibernateDemoApplication.java
@@ -0,0 +1,14 @@
+package org.mvnsearch.spring.boot.hibernate.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * hibernate test application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class HibernateDemoApplication {
+
+}
