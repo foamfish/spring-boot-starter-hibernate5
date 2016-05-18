16ebe3b6a2616c6c9722df548c3da932f417edd1
linux_china <linux_china@hotmail.com>
2016-05-18 10:38:52 +0800

    country

---
+++ src/test/java/org/mvnsearch/spring/boot/hibernate/demo/model/Country.java
@@ -0,0 +1,44 @@
+package org.mvnsearch.spring.boot.hibernate.demo.model;
+
+import javax.persistence.*;
+
+/**
+ * country
+ *
+ * @author linux_china
+ */
+@Entity(name = "Country")
+@Table(name = "countries")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "iso_code")
+    private String isoCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsoCode() {
+        return isoCode;
+    }
+
+    public void setIsoCode(String isoCode) {
+        this.isoCode = isoCode;
+    }
+}
